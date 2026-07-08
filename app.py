4161ac275dc61aa293664ab0132a402e187ef558
Sinchana MP <sinchanamp3@gmail.com>
2026-07-08 18:27:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
-from flask import Flask
-import random
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-secret_number = random.randint(1, 10)
-
-@app.route("/<int:guess>")
-def guess(guess):
-    if guess < secret_number:
-        return "Too Low! Try a bigger number."
-    elif guess > secret_number:
-        return "Too High! Try a smaller number."
-    else:
-        return "🎉 Correct! You guessed it!"
-
 @app.route("/")
 def home():
-    return """
-    <h1>Number Guessing Game</h1>
-    <p>Guess a number from 1 to 10.</p>
-    <p>Example: add /5 to the URL</p>
-    """
+    return render_template("index.html")
 
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["message"]
+
+    return f"""
+    <h1>Thank You {name}!</h1>
+    <p>Your email: {email}</p>
+    <p>Your message: {message}</p>
+    """
+    
 if __name__ == "__main__":
     app.run(debug=True)
